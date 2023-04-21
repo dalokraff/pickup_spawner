d6be1d69c194bd8b1d2a4d0f90d07612ec3fce0c
deluxghost <deluxghost@users.noreply.github.com>
2023-04-21 18:27:22 +0800

    Update pickup_spawner_localization.lua

--- scripts/mods/pickup_spawner/pickup_spawner_localization.lua
+++ scripts/mods/pickup_spawner/pickup_spawner_localization.lua
@@ -1,26 +1,34 @@
 return {
 	mod_description = {
-		en = "Let's you spawn pickups in the Training room",
+		en = "Let you spawn pickups in the Training room",
+		["zh-cn"] = "在训练场生成可拾取物品",
 	},
 	mod_name = {
 		en = "Pickup Spawner",
+		["zh-cn"] = "拾取物生成",
 	},
     spawn_pickup = {
 		en = "Spawn Pickup",
+		["zh-cn"] = "生成拾取物",
 	},
     spawn_pickup_tooltip = {
-		en = "keybind to spawn pickup",
+		en = "Keybind to spawn pickup",
+		["zh-cn"] = "生成拾取物的快捷键",
 	},
     next_pickup = {
 		en = "Next Pickup",
+		["zh-cn"] = "下一项拾取物",
 	},
     next_pickup_tooltip = {
-		en = "keybind to cylce pickup forwards",
+		en = "Keybind to cycle pickup forwards",
+		["zh-cn"] = "切换下一项拾取物的快捷键",
 	},
     prev_pickup = {
 		en = "Prev Pickup",
+		["zh-cn"] = "上一项拾取物",
 	},
     prev_pickup_tooltip = {
-		en = "keybind to cylce pickup backwards",
+		en = "Keybind to cycle pickup backwards",
+		["zh-cn"] = "切换上一项拾取物的快捷键",
 	},
 }
